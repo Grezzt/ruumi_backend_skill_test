5937b0d3e4f909a5f4579dc5ef33158056b36eae
Grezzt <andhika.prasetyo105@gmail.com>
2026-07-01 21:48:30 +0700

    feat: implement rate limit with ip, max request is 20 per 2 minutes

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -12,6 +14,23 @@ import "./workers/expiration.worker";
 
 const app = express();
 
+app.use(cors()); // Enable CORS for all origins
+
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 2 minutes
+  max: 20, // Limit each IP to 20 requests per windowMs
+  message: {
+    success: false,
+    code: 429,
+    type: 'ERR_TOO_MANY_REQUESTS',
+    message: 'Too many requests from this IP, please try again after 2 minutes',
+    data: null
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use(limiter); // Apply rate limiter to all requests
 const swaggerDocument = YAML.load(path.join(__dirname, "../swagger.yaml"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
